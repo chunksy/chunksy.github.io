697127cfdf71972b1836598e85be9ee3a2ec9884
chunksy <45887084+chunksy@users.noreply.github.com>
2024-03-25 13:54:01 -0400

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function openTab(event, tabName) {
+    const tabcontent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+}s
